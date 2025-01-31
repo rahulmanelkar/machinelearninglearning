0b6852bec43eb18b757506514be833ebdcefdc19
Rahul Manelkar <rahul.manelkar@gmail.com>
2025-01-30 20:15:42 -0800

    starting numpy practice

---
+++ numpy_practice.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def swap_rows(M, row_index_1, row_index_2):
+    """
+    Swap rows in the given matrix.
+
+    Parameters:
+    - matrix (numpy.array): The input matrix to perform row swaps on.
+    - row_index_1 (int): Index of the first row to be swapped.
+    - row_index_2 (int): Index of the second row to be swapped.
+    """
+
+    # Copy matrix M so the changes do not affect the original matrix. 
+    M = M.copy()
+    # Swap indexes
+    M[[row_index_1, row_index_2]] = M[[row_index_2, row_index_1]]
+    return M
+
